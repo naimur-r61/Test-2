bed6ba454b077f1e5a6d33269e5ac406b11571fd
Naimur <naimur.r61n@gmail.com>
2023-03-05 19:15:08 +0600

    style done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
 
@@ -33,23 +32,50 @@ function App() {
 
   }, [])
 
+  // #DADADA
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <table className="my-table">
+        <thead>
+          <tr>
+            <th>Column 1</th>
+            <th>Column 2</th>
+            <th>Column 3</th>
+            <th>Column 4</th>
+            <th>Column 5</th>
+            <th>Column 6</th>
+            <th>Column 7</th>
+            <th>Column 8</th>
+            <th>Column 9</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Row 1, Column 1</td>
+            <td>Row 1, Column 2</td>
+            <td>Row 1, Column 3</td>
+            <td>Row 1, Column 4</td>
+            <td>Row 1, Column 5</td>
+            <td>Row 1, Column 6</td>
+            <td>Row 1, Column 7</td>
+            <td>Row 1, Column 8</td>
+            <td>Row 1, Column 9</td>
+          </tr>
+          <tr>
+            <td>Row 2, Column 1</td>
+            <td>Row 2, Column 2</td>
+            <td>Row 2, Column 3</td>
+            <td>Row 2, Column 4</td>
+            <td>Row 2, Column 5</td>
+            <td>Row 2, Column 6</td>
+            <td>Row 2, Column 7</td>
+            <td>Row 2, Column 8</td>
+            <td>Row 2, Column 9</td>
+          </tr>
+          {/* add more rows as needed */}
+        </tbody>
+      </table>
     </div>
   );
 }
